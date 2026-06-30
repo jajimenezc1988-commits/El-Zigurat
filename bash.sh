67c03ff5acaadfe4572e6685247d12392b4f9af0
JApolo <ja.jimenezc1988@gmail.com>
2026-06-30 07:05:22 -0600

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+git init
+git add Zigurat.py adam_con_inegi_agregado.csv
+git commit -m "Zigurat - Sistema unificado de patrones puros"
+git remote add origin https://github.com/tu-usuario/El-Zigurat.git
+git push -u origin main
